17d51d5e547df3dd2263f390f95bd96168cb6cbe
shailesh-sangekar <shailesh.sangekar>
2020-09-07 15:30:25 +0530

    Base Application commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CORE_TEST_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
